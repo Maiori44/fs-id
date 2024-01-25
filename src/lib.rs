e5d1da2525b408b92b29532d0230f0ba884678ef
Maiori44 <felice.dangelo2013@gmail.com>
2024-01-25 09:38:02 +0100

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,30 @@ use std::{fs::File, io, path::Path};
 #[cfg_attr(unix, path = "unix.rs")]
 mod sys;
 
-pub trait GetID {
-	fn get_id(&self) -> io::Result<FileID>;
-}
-
 #[derive(Debug, PartialEq, Eq)]
 pub struct FileID (sys::FileIDImpl);
 
 impl FileID {
 	pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
 		File::open(path)?.get_id()
+	}
+}
+
+pub trait GetID {
+	fn get_id(&self) -> io::Result<FileID>;
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::FileID;
+
+	#[test]
+	fn check_comparisons() -> std::io::Result<()> {
+		let id1 = FileID::new("Cargo.toml")?;
+		let id2 = FileID::new("Cargo.toml")?;
+		let id3 = FileID::new("LICENSE")?;
+		assert_eq!(id1, id2);
+		assert_ne!(id1, id3);
+		Ok(())
 	}
 }
